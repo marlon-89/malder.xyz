891aa4204672baa5c0bbebf2fc560caff8fe7b7c
yaroslav.l <yaroslav.l@kultprosvet.net>
2025-02-26 21:25:03 +0200

    Add under development message

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
 
 <template>
   <header>
-
+    <under-development/>
   </header>
 </template>
 
---
+++ components/UnderDevelopment.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="under-development">
+    <div class="under-development__inner">
+      <span>
+        <v-icon icon="fas fa-wrench" color="background"/>
+        Website is under development
+        <v-icon icon="fas fa-rocket" color="background"/>
+      </span>
+      <span>
+        <v-icon icon="fas fa-wrench" color="background"/>
+        Website is under development
+        <v-icon icon="fas fa-rocket" color="background"/>
+      </span>
+      <span>
+        <v-icon icon="fas fa-wrench" color="background"/>
+        Website is under development
+        <v-icon icon="fas fa-rocket" color="background"/>
+      </span>
+    </div>
+    <div class="under-development__inner">
+      <span>
+        <v-icon icon="fas fa-wrench" color="background"/>
+        Website is under development
+        <v-icon icon="fas fa-rocket" color="background"/>
+      </span>
+      <span>
+        <v-icon icon="fas fa-wrench" color="background"/>
+        Website is under development
+        <v-icon icon="fas fa-rocket" color="background"/>
+      </span>
+      <span>
+        <v-icon icon="fas fa-wrench" color="background"/>
+        Website is under development
+        <v-icon icon="fas fa-rocket" color="background"/>
+      </span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.under-development {
+  overflow: hidden;
+  white-space: nowrap;
+  position: relative;
+  background: #ffff46;
+  color: #111111;
+  padding: 8px 0;
+  font-size: 14px;
+  letter-spacing: 1.5px;
+  font-weight: bold;
+  width: 100%;
+
+  &__inner {
+    display: inline-flex;
+    min-width: 100%;
+    animation: under-development 30s linear infinite;
+
+    span {
+      display: inline-block;
+      padding-left: 250px;
+    }
+  }
+}
+
+@keyframes under-development {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+}
+</style>
