30e1fef02092ea780ac68ccfb3185eab5dd8587b
yaroslav.l <yaroslav.l@kultprosvet.net>
2025-03-02 19:04:34 +0200

    Add Jackal validator

--- data/chains.ts
+++ data/chains.ts
@@ -12,5 +12,12 @@ export const chains = [
 		description: 'The Incubator of the Interchain. Smart Contracts × Community',
 		website: 'https://junonetwork.io/',
 		validator_link: 'https://ping.pub/juno/staking/junovaloper1jw8rl2t8g530yzwv6pn6xt5xcjkwfz8hzm5asq'
+	},
+	{
+		name: 'Jackal',
+		image: '/images/chains/jackal.png',
+		description: 'Jackal is a blockchain that delivers decentralized data storage to individuals, businesses, and other blockchains.',
+		website: 'https://www.jackalprotocol.com/',
+		validator_link: 'https://ping.pub/jackal/staking/jklvaloper1um6780jx9h5yd70pkkrjt3ps373vjddug9uwk8'
 	}
 ]
